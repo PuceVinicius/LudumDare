958a1ae7275a876655de06612f88b84ad28790b8
Vinícius Moreno <vinicius.moreno.ismart@gmail.com>
2019-10-05 03:35:41 -0300

    character movement and properties

--- Game/scenes/KinematicBody2D.gd
+++ Game/scenes/KinematicBody2D.gd
@@ -1,21 +1,61 @@
 extends KinematicBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
+# Character properties
+var toxicity
+var immunity
+var hunger
+var thirst
+var tired
+var hp
+var happy
+#var sanity
+
+# Animation variables
+onready var ANI = get_node("AnimationPlayer")
+var movement #store the direction of the player
+var vecMove #store the movement of the player
+var SPEED = 150 #speed ratio
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	movement = "idleDown"
+	vecMove = Vector2(0, 0)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("ui_right"):
-		self.move_and_collide(Vector2(1, 0))
-	if Input.is_action_pressed("ui_left"):
-		self.move_and_collide(Vector2(-1, 0))
-	if Input.is_action_pressed("ui_up"):
-		self.move_and_collide(Vector2(0, -1))
-	if Input.is_action_pressed("ui_down"):
-		self.move_and_collide(Vector2(0, 1))
+	update_sprite_and_move(delta)
+	pass
+	
+
+func update_sprite_and_move(delta):
+	button_action("ui_right", Vector2(1, 0))
+	button_action("ui_left", Vector2(-1, 0))
+	button_action("ui_up", Vector2(0, -1))
+	button_action("ui_down", Vector2(0, 1))
+		
+	if vecMove.x != 0 or vecMove.y != 0:
+		self.move_and_collide(vecMove*delta*SPEED)
+		if vecMove.y > 0:
+			movement = "moveDown"
+		elif vecMove.y < 0:
+			movement = "moveUp"
+		elif vecMove.x > 0:
+			movement = "moveRight"
+		elif vecMove.x < 0:
+			movement = "moveLeft"
+	else:
+		if movement == "moveDown": movement = "idleDown"
+		elif movement == "moveUp": movement = "idleUp"
+		elif movement == "moveRight": movement = "idleRight"
+		elif movement == "moveLeft": movement = "idleLeft"
+	ANI.play(movement)
+	pass
+	
+func button_action(action, vec):
+	if Input.is_action_just_pressed(action):
+		vecMove += vec
+	if Input.is_action_just_released(action):
+		vecMove -= vec
 	pass
