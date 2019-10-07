fb07c27d26be578b5c3f297e3f907c1bd955e347
Vinícius Moreno <vinicius.moreno.ismart@gmail.com>
2019-10-07 05:49:01 -0300

    collision boxes

---
+++ Game/assets/Map/city.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="city" tilewidth="16" tileheight="16" spacing="1" tilecount="1036" columns="37">
+ <image source="../Spritesheet/roguelikeCity_magenta.png" width="628" height="475"/>
+ <terraintypes>
+  <terrain name="Calçada Clara" tile="820"/>
+ </terraintypes>
+ <tile id="709" terrain=",,,0"/>
+ <tile id="710" terrain=",,0,0"/>
+ <tile id="711" terrain=",,0,"/>
+ <tile id="746" terrain=",0,,0"/>
+ <tile id="748" terrain="0,,0,"/>
+ <tile id="785" terrain="0,0,0,0"/>
+ <tile id="820" terrain=",0,,"/>
+ <tile id="821" terrain="0,0,,"/>
+ <tile id="822" terrain="0,,,"/>
+</tileset>
